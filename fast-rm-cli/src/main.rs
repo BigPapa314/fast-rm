46ffaa80b94ff6532bdde5328d45c8f4732fbbf1
Thomas Kilian <Thomas.Kilian@Helmut-Fischer.com>
2022-07-11 16:17:09 +0200

    Add tokio implementation

--- fast-rm-cli/src/main.rs
+++ fast-rm-cli/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use fast_rm::{Config, FastRm};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let args: Vec<String> = std::env::args().collect();
+    let config = Config::new(args[1].clone());
+    let frm = FastRm::new(config);
+
+    frm.run().await?;
+
+    Ok(())
 }
--- fast-rm/src/lib.rs
+++ fast-rm/src/lib.rs
@@ -1,8 +1,36 @@
+use anyhow::Result;
+
+pub struct Config {
+    path: String,
+}
+
+impl Config {
+    pub fn new(path: impl Into<String>) -> Self {
+        Self { path: path.into() }
+    }
+}
+
+pub struct FastRm {
+    config: Config,
+}
+
+impl FastRm {
+    pub fn new(config: Config) -> Self {
+        Self { config }
+    }
+
+    pub async fn run(self) -> Result<()> {
+        tokio::fs::remove_dir_all(self.config.path).await?;
+        Ok(())
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    #[tokio::test]
+    async fn run() {
+        let config = crate::Config::new("D:/Temp/rm");
+        let frm = crate::FastRm::new(config);
+        assert_eq!(frm.run().await.unwrap(), ());
     }
 }
